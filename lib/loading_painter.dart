4675cd9ea7ed244f6f99c5d9c72c911588aa4583
han <342254689@qq.com>
2022-05-27 21:46:40 +0800

    💡 添加注释、优化代码

--- lib/loading_painter.dart
+++ lib/loading_painter.dart
@@ -13,11 +13,8 @@ class LoadingPainter extends CustomPainter {
     ..color = Colors.white70
     ..strokeWidth = 1.5;
 
-  /// 所有的初始点位
-  ///   0  1
-  /// 7      2
-  /// 6      3
-  ///   5  4
+  /// 所有的点位
+  /// [originOffsets]相当于地图，内容是不变的
   late final List<Offset> originOffsets = <Offset>[
     Offset(shorterLength, 0),
     Offset(baseLength + shorterLength, 0),
@@ -28,6 +25,15 @@ class LoadingPainter extends CustomPainter {
     Offset(0, baseLength + shorterLength),
     Offset(0, shorterLength),
   ];
+
+  /// 初始化每个pointer的起点终点
+  /// 设定：index为偶数的每次移动index+1,index为奇数的每次移动index+3
+  /// 使用[%]模运算达到循环链表的效果，改变pointer对象对应的值，一直循环下去
+  /// 初始    0   1      一次后    5   0
+  /// 初始  7       2    一次后  6       7
+  /// 初始  6       3    一次后  3       2
+  /// 初始    5   4      一次后    4   1
+  /// 以此类推
   late final List<CustomPointer> pointers = List.generate(
     originOffsets.length,
     (int index) {
@@ -41,27 +47,34 @@ class LoadingPainter extends CustomPainter {
     },
   );
 
+  /// 根据[progress]得到实时的位置。[progress]的值不断变化，就形成动画了
+  Offset realtimeOffsetOfPointer(CustomPointer customPointer) =>
+      Offset.lerp(customPointer.start, customPointer.end, progress.value) ??
+      customPointer.end;
+
   @override
   void paint(Canvas canvas, Size size) {
+    // 往左上方移动，让整体显示效果居中
     canvas.translate(-(2 * shorterLength + baseLength) / 2,
         -(2 * shorterLength + baseLength) / 2);
+    // 先画线
     for (int i = 0; i < pointers.length; i++) {
       final CustomPointer pointer = pointers[i];
       canvas.drawLine(
-        pointer.current(progress.value),
-        pointers[(i + 1) % pointers.length].current(progress.value),
+        realtimeOffsetOfPointer(pointer),
+        realtimeOffsetOfPointer(pointers[(i + 1) % pointers.length]),
         linePaint,
       );
     }
-    for (int i = 0; i < pointers.length; i++) {
-      final CustomPointer pointer = pointers[i];
+    for (final CustomPointer pointer in pointers) {
       canvas.drawCircle(
-        pointer.current(progress.value),
+        realtimeOffsetOfPointer(pointer),
         circleRadius,
         circlePaint,
       );
     }
-
+    // 重新设定每个pointer的起点终点
+    // 使用[%]模运算达到循环链表的效果，改变pointer对象对应的属性的值，一直循环下去
     if (progress.value == 1) {
       for (CustomPointer pointer in pointers) {
         pointer.start = pointer.end;
@@ -74,9 +87,7 @@ class LoadingPainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
 class CustomPointer {
@@ -90,8 +101,4 @@ class CustomPointer {
   Offset end;
   // 是否是旋转的点（相对应的是直接跳跃的点）
   bool isRotated;
-
-  Offset current(double progress) {
-    return Offset.lerp(start, end, progress) ?? end;
-  }
 }
